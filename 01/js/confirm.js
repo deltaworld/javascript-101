a619bf62707e0645703ca31ec47ea51667e4e446
“Tareq <itrainit@gmail.com>
2015-08-10 14:29:03 +0000

    confirm

---
+++ 01/js/confirm.js
@@ -0,0 +1,7 @@
+if ( confirm("Click OK please") ) { // condition is true or false.
+    // if true do these instructions
+    alert("You clicked OK. Well Done");
+} else {
+    // if false do here.
+    alert("You clicked Cancel. Why did you do that?");
+}

00070b1f1ab686c0feb88b5b39f00f2b77417829
“Tareq <itrainit@gmail.com>
2015-08-10 14:11:07 +0000

    update comments

--- 01/js/external.js
+++ 01/js/external.js
@@ -2,6 +2,10 @@
 // onload is our eventListener will only perform the writeMessage function when 
 // the page has *finished* loading.
 // writeMessage is our function that we will run.
+// as this function is associated with an eventListener it is treated as an EventHandler.
+// In this case the function does not need to be called immediately when parsed but only
+// when the event has been triggered. Hence we do not write the parentheses as the parentheses
+// means to call the function when parsed (i.e. when read).
 
 window.onload = writeMessage;
 
@@ -13,5 +17,10 @@ i.e. what is between the <h1> and the </h1> using
 the property .innerHTML
 */
 function writeMessage() {
-    document.getElementById("helloMessage").innerHTML = "Write this message";
+    //document is object
+    //getElementById("helloMessage") is a sub-object <h1 id='helloMessage'></h1>
+    //.innerHTML is a property of that sub-object
+    //changing the property by assignment of = to whatever value we have after.
+    document.getElementById("helloMessage").innerHTML = "Here we are!";
+    
 } 

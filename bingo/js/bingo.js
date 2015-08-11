acddb703f521546c078541e143793627ff4d7737
“Tareq <itrainit@gmail.com>
2015-08-11 14:50:39 +0000

    Refactorised Random Number

--- bingo/js/bingo.js
+++ bingo/js/bingo.js
@@ -1,24 +1,38 @@
 window.onload = function () {
  
-    for (var i = 0; i < 24; i++) {
-        // refactored to a function that sets the square with a number.
-        setSquare(i);    
-    }
-    
-    // JS: Closure
-    function setSquare(i) {
+    if (document.getElementById ) { // truthy statement
+        for (var i = 0; i < 24; i++) {
+            // refactored to a function that sets the square with a number.
+            setSquare(i);    
+        }
         
-        // Refactor Current Square to an independent local variable.
-        var currSquare = "square" + i;  // "square0"
-        
-        // grabs the td element from the html
-        var currSquareElement = document.getElementById(currSquare); // <td id="square0"></td>
-        
-        // creates a random number between 1 - 75
-        var newNum = Math.floor(Math.random() * 75) + 1;  // randNu = 73
-        
-        //populates the elemnt  with random number newNum
-        currSquareElement.innerHTML = newNum; // <td id="square0">73</td>
+        // JS: Closure
+        function setSquare(i) {
+            
+            function getRandomNumber(max) {
+                // creates a random number between 1 - 75
+                var newNum = Math.floor(Math.random() * max) + 1;  // randNu = 73
+                return newNum;
+            }
+            
+            // Refactor Current Square to an independent local variable.
+            var currSquare = "square" + i;  // "square0"
+            
+            // The index refers to the square.no and the value of the array tells us the column number.
+            var colPlace = [0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4];
+            
+            // This gets the current element from the HTML page.
+            var currSquareElement = document.getElementById(currSquare); // <td id="square0"></td>
+            
+            
+            
+            //finds the required element and populates it with random number newNum
+            currSquareElement.innerHTML = getRandomNumber(75); // <td id="square0">73</td>
+        }
+    } else {
+        alert("Sorry, your browser doesn't support this script");
     }
 }
+
+
 

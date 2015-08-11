75dbcd2e9b5fc03f8c8144b986b2e94096b0bf71
“Tareq <itrainit@gmail.com>
2015-08-11 11:56:34 +0000

    refactor with setSquare variables

--- bingo/js/bingo.js
+++ bingo/js/bingo.js
@@ -4,13 +4,21 @@ window.onload = function () {
         // refactored to a function that sets the square with a number.
         setSquare(i);    
     }
-}
-
-function setSquare(argument) {
-    // creates a random number between 1 - 75
-    var newNum = Math.floor(Math.random() * 75) + 1;
     
-    //finds the required element and populates it with random number newNum
-    document.getElementById("square" + argument).innerHTML = newNum;
+    // JS: Closure
+    function setSquare(i) {
+        
+        // Refactor Current Square to an independent local variable.
+        var currSquare = "square" + i;  // "square0"
+        
+        // grabs the td element from the html
+        var currSquareElement = document.getElementById(currSquare); // <td id="square0"></td>
+        
+        // creates a random number between 1 - 75
+        var newNum = Math.floor(Math.random() * 75) + 1;  // randNu = 73
+        
+        //populates the elemnt  with random number newNum
+        currSquareElement.innerHTML = newNum; // <td id="square0">73</td>
+    }
 }
 
